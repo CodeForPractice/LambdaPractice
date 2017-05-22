cdf1b210ca38486f81e80ec3d1db0bb49c041381
叶俊权 <425527169@qq.com>
2017-05-22 22:18:24 +0800

    test

--- LambdaPractice/Program.cs
+++ LambdaPractice/Program.cs
@@ -1,6 +1,10 @@
 ﻿using JQ.LambdaResolve;
+using NLog;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
@@ -11,100 +15,24 @@ namespace LambdaPractice
 {
     class Program
     {
+        static StringBuilder builder = new StringBuilder();
+        static int currentParameterIndex = 0;
+        static List<SqlParameter> dbParameterList = new List<SqlParameter>();
+
         static void Main(string[] args)
         {
-            //Expression firstArg = Expression.Constant(2);
-            //Expression secondArg = Expression.Constant(3);
-            //Expression add = Expression.Add(firstArg, secondArg);
-            //Console.WriteLine(add);
 
-            //Expression firstArg = Expression.Constant(2);
-            //Expression secondArg = Expression.Constant(3);
-            //Expression add = Expression.Add(firstArg, secondArg);
+            //var expression1 = ToExpression4(5);
+            int[] ages = new int[] { 1, 2, 5, 67 };
+            //Expression<Func<User, bool>> expression2 = m => (m.Age == 10 && m.Address.StartsWith("浙江")) || (m.Sex == 0 && m.Age >= ages[1] || "" == m.Address);
+            Expression<Func<User, bool>> expression2 = m => m.Age >= ages[1];
 
-            //Func<int> compiled = Expression.Lambda<Func<int>>(add).Compile();
-            //Console.WriteLine(compiled());
-
-            //Expression<Func<int>> return5 = () => 5;//Lambda表达式
-            //Func<int> compiled1 = return5.Compile();
-            //Console.WriteLine(compiled1());
-
-            //Expression<Func<string, string, bool>> expression = (x, y) => x.StartsWith(y);
-            //var compiled = expression.Compile();
-
-            //Console.WriteLine(compiled("First", "Second"));
-            //Console.WriteLine(compiled("First", "Fir"));
-
-            //MethodInfo method = typeof(string).GetMethod("StartsWith", new[] { typeof(string) });//获取方法名为StartsWith，参数为string的公共方法
-            //var target = Expression.Parameter(typeof(string), "x");
-            //var methodArg = Expression.Parameter(typeof(string), "y");
-            //Expression[] methodArgs = new[] { methodArg };
-
-            ////Call(Expression instance, MethodInfo method, params Expression[] arguments)
-            //Expression call = Expression.Call(target, method, methodArgs);//x.StartsWith(y),以上部件创建CallExpression
-
-            //var lambdaParameters = new[] { target, methodArg };//这里使用的参数顺序就是调用委托所使用的参数顺序
-            //var lambda = Expression.Lambda<Func<string, string, bool>>(call, lambdaParameters);//(x,y)=>x.StartsWith(y),lambdaParameters填充call集合
-            //var compiled = lambda.Compile();//生成lambda表达式的委托
-
-            //Console.WriteLine(compiled("First", "csend"));
-            //Console.WriteLine(compiled("First", "Fir"));
-
-            //ParameterExpression parameterExp1 = Expression.Parameter(typeof(int), "x");
-            //ParameterExpression parameterExp2 = Expression.Parameter(typeof(int), "y");
-            //BinaryExpression binaryExpression = Expression.Multiply(parameterExp1, parameterExp2);
-            //var func = Expression.Lambda<Func<int, int, int>>(binaryExpression, parameterExp1, parameterExp2).Compile();
-            //var result = func(5, 2);
-            //Console.WriteLine(result.ToString());
-
-            //ParameterExpression parameterExp3 = Expression.Parameter(typeof(int), "x");
-            //ParameterExpression parameterExp4 = Expression.Parameter(typeof(int), "y");
-            //BinaryExpression binaryExpression2 = Expression.Divide(parameterExp3, parameterExp4);
-            //var func2 = Expression.Lambda<Func<int, int, int>>(binaryExpression2, parameterExp3, parameterExp4).Compile();
-            //var result2 = func2(5, 2);
-            //Console.WriteLine(result2.ToString());
-
-            //BinaryExpression last = Expression.Add(binaryExpression, binaryExpression2);
-
-            //var lastFunc = Expression.Lambda<Func<int, int, int, int, int>>(last, parameterExp1, parameterExp2, parameterExp3, parameterExp4).Compile();
-            //var lastResult = lastFunc(5, 2, 5, 2);
-            //Console.WriteLine(lastResult.ToString());
-
-            //ParameterExpression parameterExp5 = Expression.Parameter(typeof(int));
-            //var blockExp = Expression.Block( Expression.AddAssign(parameterExp5, Expression.Constant(10)), parameterExp5);
-            //Console.WriteLine(Expression.Lambda<Func<int,int>>(blockExp, parameterExp5).Compile().Invoke(10).ToString());
-
-            //LabelTarget returnTarget = Expression.Label(typeof(Int32));
-            //LabelExpression returnLabel = Expression.Label(returnTarget, Expression.Constant(10, typeof(Int32)));
-
-            //// 为输入参加+10之后返回
-            //ParameterExpression inParam3 = Expression.Parameter(typeof(int));
-            //BlockExpression block3 = Expression.Block(
-            //    Expression.AddAssign(inParam3, Expression.Constant(10)),
-            //    Expression.Return(returnTarget, inParam3),
-            //    returnLabel);
-
-            //Expression<Func<int, int>> expr3 = Expression.Lambda<Func<int, int>>(block3, inParam3);
-            //Console.WriteLine(expr3.Compile().Invoke(20));
-
-            //Expression<Func<User, bool>> func = m => !(m.Age == 1&&m.Sex<11)&&m.Address.Contains("浙江");
-            //var array = new int[] { 1,2,3};
-            //Expression<Func<User, bool>> func = m => array.Contains(m.Age);
-            //LambdaResolve resolve = new LambdaResolve();
-            //var result = resolve.Resolve(func.Body);
-            //Console.WriteLine(result);
-            //var str=ExpressionHelper.GetSqlByExpression(func.Body);
-            //Console.WriteLine(str);
-
-
-
-            //Expression<Func<User, bool>> func = m => m.Age == 1;
-            //var exp = Evaluator.PartialEval(func);
-            //Console.WriteLine(exp.ToString());
-
-            var expression1 = ToExpression3();
-            ResolveExpression(expression1.Body);
-
+            ResolveExpression(expression2.Body);
+            Debug(builder.ToString());
+            foreach (var item in dbParameterList)
+            {
+                Debug($"{item.ParameterName},{item.Value},{item.SqlDbType},{item.Size}");
+            }
             Console.Read();
         }
 
@@ -114,17 +42,210 @@ namespace LambdaPractice
             Console.WriteLine("类型" + nodeType.ToString());
             if (expression is BinaryExpression)
             {
+                Warn("BinaryExpression");
                 ResolveBinaryExpression(expression as BinaryExpression);
+            }
+            else if (expression is BlockExpression)
+            {
+                Warn("BlockExpression");
+            }
+            else if (expression is ConditionalExpression)
+            {
+                Warn("ConditionalExpression");
+            }
+            else if (expression is ConstantExpression)
+            {
+                Warn("ConstantExpression");
+                ResolveConstantExpression(expression as ConstantExpression);
+            }
+            else if (expression is DebugInfoExpression)
+            {
+                Warn("DebugInfoExpression");
+            }
+            else if (expression is DefaultExpression)
+            {
+                Warn("DefaultExpression");
+            }
+            else if (expression is DynamicExpression)
+            {
+                Warn("DynamicExpression");
+            }
+            else if (expression is GotoExpression)
+            {
+                Warn("GotoExpression");
+            }
+            else if (expression is IndexExpression)
+            {
+                Warn("IndexExpression");
+            }
+            else if (expression is InvocationExpression)
+            {
+                Warn("InvocationExpression");
+            }
+            else if (expression is ListInitExpression)
+            {
+                Warn("ListInitExpression");
+            }
+            else if (expression is LambdaExpression)
+            {
+                Warn("LambdaExpression");
+            }
+            else if (expression is LoopExpression)
+            {
+                Warn("LoopExpression");
+            }
+            else if (expression is MemberExpression)
+            {
+                Warn("MemberExpression");
+                ResolveMemberExpression(expression as MemberExpression);
+            }
+            else if (expression is MemberInitExpression)
+            {
+                Warn("MemberInitExpression");
+            }
+            else if (expression is MethodCallExpression)
+            {
+                Warn("MethodCallExpression");
+                ResolveMethodCallExpression(expression as MethodCallExpression);
+            }
+            else if (expression is NewArrayExpression)
+            {
+                Warn("NewArrayExpression");
+            }
+            else if (expression is NewExpression)
+            {
+                Warn("NewExpression");
+            }
+            else if (expression is ParameterExpression)
+            {
+                Warn("ParameterExpression");
+                ResolveParameterExpression(expression as ParameterExpression);
+            }
+            else if (expression is SwitchExpression)
+            {
+                Warn("SwitchExpression");
+            }
+            else if (expression is TryExpression)
+            {
+                Warn("TryExpression");
+            }
+            else if (expression is RuntimeVariablesExpression)
+            {
+                Warn("RuntimeVariablesExpression");
             }
         }
 
         static void ResolveBinaryExpression(BinaryExpression expression)
         {
-            var expressionLeft = expression.Left;
-            ResolveExpression(expressionLeft);
-            Console.WriteLine(expression.NodeType);
-            var expressionRight = expression.Right;
-            ResolveExpression(expressionRight);
+            if (expression.NodeType == ExpressionType.ArrayIndex)
+            {
+                throw new NotSupportedException("ArrayIndex");
+            }
+            else
+            {
+                builder.Append("(");
+                var expressionLeft = expression.Left;
+                ResolveExpression(expressionLeft);
+                //Console.WriteLine(expression.NodeType);
+                ResolveExpressionType(expression.NodeType);
+                var expressionRight = expression.Right;
+                ResolveExpression(expressionRight);
+                builder.Append(")");
+            }
+        }
+        static void ResolveParameterExpression(ParameterExpression expression)
+        {
+            Console.WriteLine(expression.Name);
+            Console.WriteLine(expression.Type.Name);
+        }
+
+        static void ResolveMemberExpression(MemberExpression expression)
+        {
+            Info(expression.Member.Name);
+            builder.Append(expression.Member.Name);
+        }
+
+        static void ResolveConstantExpression(ConstantExpression expression)
+        {
+            string parameterName = $"@Parameter_{currentParameterIndex.ToString()}";
+            currentParameterIndex++;
+            builder.Append(parameterName);
+            dbParameterList.Add(new SqlParameter { Value = expression.Value, SqlDbType = TypeString2SqlType(expression.Type.Name.ToLower()), ParameterName = parameterName });
+        }
+
+        static void ResolveMethodCallExpression(MethodCallExpression expression)
+        {
+            if (expression.Object != null)
+            {
+                ResolveExpression(expression.Object);
+            }
+            string methodName = expression.Method.Name;
+            Debug($"MethodName:{methodName}");
+            switch (methodName)
+            {
+                case "StartsWith":
+                    ResolveMethodStartsWith(expression);
+                    break;
+            }
+
+        }
+        static void ResolveMethodStartsWith(MethodCallExpression expression)
+        {
+            builder.Append(" LIKE '%'+");
+            if (expression.Arguments != null)
+            {
+                Error("方法参数:");
+                foreach (var item in expression.Arguments)
+                {
+                    ResolveExpression(item);
+                }
+                Error("===");
+            }
+        }
+
+
+        public static void ResolveExpressionType(ExpressionType expressionType)
+        {
+            Warn(expressionType.ToString());
+            switch (expressionType)
+            {
+                case ExpressionType.Add:
+                case ExpressionType.AddAssign:
+                case ExpressionType.AddAssignChecked:
+                case ExpressionType.AddChecked:
+                    builder.Append("+");
+                    break;
+                case ExpressionType.And:
+                case ExpressionType.AndAlso:
+                case ExpressionType.AndAssign:
+                    builder.Append(" AND ");
+                    break;
+
+                case ExpressionType.LessThan:
+                    builder.Append(" < ");
+                    break;
+                case ExpressionType.LessThanOrEqual:
+                    builder.Append(" <= ");
+                    break;
+                case ExpressionType.Equal:
+                    builder.Append(" = ");
+                    break;
+                case ExpressionType.NotEqual:
+                    builder.Append(" != ");
+                    break;
+                case ExpressionType.Not:
+                    builder.Append(" NOT ");
+                    break;
+                case ExpressionType.GreaterThan:
+                    builder.Append(" > ");
+                    break;
+                case ExpressionType.GreaterThanOrEqual:
+                    builder.Append(" >= ");
+                    break;
+                case ExpressionType.OrElse:
+                    builder.Append(" OR ");
+                    break;
+            }
         }
 
         public static Expression<Func<int, int, int, int, int>> ToExpression3()
@@ -144,6 +265,152 @@ namespace LambdaPractice
             MemberExpression member1 = Expression.Property(parameter1, "Age");
             BinaryExpression binary = Expression.LessThanOrEqual(member1, Expression.Constant(age, typeof(int)));
             return Expression.Lambda<Func<User, bool>>(binary, parameter1);
+        }
+
+        /// <summary>
+        /// 根据类型对应类型获取对应数据库对应的类型
+        /// </summary>
+        /// <param name="infoTypeString">类型类型</param>
+        /// <returns>数据库对应的类型</returns>
+        public static SqlDbType TypeString2SqlType(string infoTypeString)
+        {
+            SqlDbType dbType = SqlDbType.Binary;//默认为Object
+
+            switch (infoTypeString)
+            {
+                case "int16":
+                case "int32":
+                    dbType = SqlDbType.Int;
+                    break;
+
+                case "string":
+                case "varchar":
+                    dbType = SqlDbType.NVarChar;
+                    break;
+
+                case "boolean":
+                case "bool":
+                case "bit":
+                    dbType = SqlDbType.Bit;
+                    break;
+
+                case "datetime":
+                    dbType = SqlDbType.DateTime;
+                    break;
+
+                case "decimal":
+                    dbType = SqlDbType.Decimal;
+                    break;
+
+                case "float":
+                    dbType = SqlDbType.Float;
+                    break;
+
+                case "image":
+                    dbType = SqlDbType.Image;
+                    break;
+
+                case "money":
+                    dbType = SqlDbType.Money;
+                    break;
+
+                case "ntext":
+                    dbType = SqlDbType.NText;
+                    break;
+
+                case "nvarchar":
+                    dbType = SqlDbType.NVarChar;
+                    break;
+
+                case "smalldatetime":
+                    dbType = SqlDbType.SmallDateTime;
+                    break;
+
+                case "smallint":
+                    dbType = SqlDbType.SmallInt;
+                    break;
+
+                case "text":
+                    dbType = SqlDbType.Text;
+                    break;
+
+                case "int64":
+                case "bigint":
+                    dbType = SqlDbType.BigInt;
+                    break;
+
+                case "binary":
+                    dbType = SqlDbType.Binary;
+                    break;
+
+                case "char":
+                    dbType = SqlDbType.Char;
+                    break;
+
+                case "nchar":
+                    dbType = SqlDbType.NChar;
+                    break;
+
+                case "numeric":
+                    dbType = SqlDbType.Decimal;
+                    break;
+
+                case "real":
+                    dbType = SqlDbType.Real;
+                    break;
+
+                case "smallmoney":
+                    dbType = SqlDbType.SmallMoney;
+                    break;
+
+                case "sql_variant":
+                    dbType = SqlDbType.Variant;
+                    break;
+
+                case "timestamp":
+                    dbType = SqlDbType.Timestamp;
+                    break;
+
+                case "tinyint":
+                    dbType = SqlDbType.TinyInt;
+                    break;
+
+                case "uniqueidentifier":
+                    dbType = SqlDbType.UniqueIdentifier;
+                    break;
+
+                case "varbinary":
+                    dbType = SqlDbType.VarBinary;
+                    break;
+
+                case "xml":
+                    dbType = SqlDbType.Xml;
+                    break;
+            }
+            return dbType;
+        }
+
+        static Logger GetLogger()
+        {
+            return NLog.LogManager.GetLogger("*");
+        }
+
+        public static void Debug(string msg)
+        {
+            GetLogger().Debug(msg);
+        }
+
+        public static void Info(string msg)
+        {
+            GetLogger().Info(msg);
+        }
+        public static void Warn(string msg)
+        {
+            GetLogger().Warn(msg);
+        }
+        public static void Error(string msg)
+        {
+            GetLogger().Error(msg);
         }
     }
     class User
