352c4c089ba4ef21bc18837a1f7de9c390b809a1
叶俊权 <425527169@qq.com>
2017-05-25 20:24:38 +0800

    test

---
+++ JQ/Expression/DataMemberType.cs
@@ -0,0 +1,27 @@
+﻿namespace JQ.Expression
+{
+    /// <summary>
+    /// Copyright (C) 2017 yjq 版权所有。
+    /// 类名：DataMemberType.cs
+    /// 类属性：公共类（非静态）
+    /// 类功能描述：数据类型
+    /// 创建标识：yjq 2017/5/25 17:24:07
+    /// </summary>
+    public enum DataMemberType
+    {
+        /// <summary>
+        /// 默认（完成组合的）
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// 字段
+        /// </summary>
+        Key = 1,
+
+        /// <summary>
+        /// 值
+        /// </summary>
+        Value = 2
+    }
+}
---
+++ JQ/Expression/ExpressionVisitor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JQ.Expression
+{
+    /// <summary>
+    /// Copyright (C) 2017 yjq 版权所有。
+    /// 类名：ExpressionVisitor.cs
+    /// 类属性：公共类（非静态）
+    /// 类功能描述：ExpressionVisitor
+    /// 创建标识：yjq 2017/5/25 17:23:23
+    /// </summary>
+    public abstract class ExpressionVisitor
+    {
+    }
+}
